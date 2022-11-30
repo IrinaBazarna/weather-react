5142a0221eeb588653737a8164c3a1c6515b0a3e
IrinaBazarna <bazarnairen@gmail.com>
2022-11-30 19:06:42 +0200

    Icon.js moved

--- Icon.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Icon(props) {
-  return <img src={props.data.weather.icon} alt={props.alt} />;
-}
--- WeatherDate.js
+++ WeatherDate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DayChange from "./DayChange";
-import Icon from "./Icon";
+import Icon from "./src/Icon";
 
 export default function WeatherDate(props) {
   return (

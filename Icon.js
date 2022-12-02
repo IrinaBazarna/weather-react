5b933f2ee1e95b376b5559636a26e51f42e470d5
IrinaBazarna <108300701+IrinaBazarna@users.noreply.github.com>
2022-12-02 23:56:20 +0200

    Delete Icon.js

--- Icon.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Icon from "./Icon";
-
-export default function Icon(props) {
-  return (
-    <img src="http://openweathermap.org/img/wn/10d@2x.png" alt={props.alt} />
-  );
-}

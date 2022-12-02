eec1b4e25df66bd1947b0e0a2321f75aa751a1bc
IrinaBazarna <108300701+IrinaBazarna@users.noreply.github.com>
2022-12-03 00:02:19 +0200

    Add files via upload

---
+++ src/Icon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Icon from "./Icon";
+
+export default function Icon(props) {
+  return (
+    <img src="http://openweathermap.org/img/wn/10d@2x.png" alt={props.alt} />
+  );
+}

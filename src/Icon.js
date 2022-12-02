b9d2a9d018d4fe24d7a447a596a34f50b7d20d2c
IrinaBazarna <108300701+IrinaBazarna@users.noreply.github.com>
2022-12-03 00:02:47 +0200

    Update Icon.js

--- src/Icon.js
+++ src/Icon.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Icon from "./Icon";
 
-export default function Icon(props) {
+
+export default function icon(props) {
   return (
     <img src="http://openweathermap.org/img/wn/10d@2x.png" alt={props.alt} />
   );
